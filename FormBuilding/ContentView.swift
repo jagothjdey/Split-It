fa1d88850765d704e7845e2a9509124135de0278
dey <jagothjyotidey91@gmail.com>
2019-11-18 00:58:41 +0100

    Initial Commit

---
+++ FormBuilding/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FormBuilding
+//
+//  Created by Jagoth Jyoti Dey on 18.11.19.
+//  Copyright © 2019 Jagoth. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
