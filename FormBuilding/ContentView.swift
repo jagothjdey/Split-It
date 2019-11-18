e66ae2cdb5385336163f3d8e65e7a66e54cb1acd
dey <jagothjyotidey91@gmail.com>
2019-11-18 03:14:51 +0100

    feat(added app icon)

--- FormBuilding/ContentView.swift
+++ FormBuilding/ContentView.swift
@@ -11,46 +11,44 @@ struct ContentView: View {
         let peopleCount = Double(numberOfPeople + 2)
         let tipSelection = Double(tipPercentages[tipPercentage])
         let orderAmount = Double(amount) ?? 0
-
+        
         let tipValue = orderAmount / 100 * tipSelection
         let grandTotal = orderAmount + tipValue
         let amountPerPerson = grandTotal / peopleCount
-
+        
         return amountPerPerson
     }
     
     var body: some View {
         NavigationView{
-            ZStack{
-                LinearGradient(gradient: Gradient(colors: [.white,.black]), startPoint: .top, endPoint: .bottom)
-                Form{
-                    Section{
-                        TextField("Enter Your Amount", text: $amount)
-                            .keyboardType(.decimalPad)
-                        
-                        Picker(selection: $numberOfPeople, label: Text("Number of People")){
-                            ForEach(2 ..< 10){
-                                Text("\($0) People")
-                            }
+            Form{
+                Section{
+                    TextField("Enter Your Amount", text: $amount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker(selection: $numberOfPeople, label: Text("Number of People")){
+                        ForEach(2 ..< 10){
+                            Text("\($0) People")
                         }
+                    }
                     .pickerStyle(DefaultPickerStyle())
-                    }
-                    
-                    Section ( header: Text("How Much Tip You Would Like To Leave")){
-                        Picker("Tip percentage", selection: $tipPercentage) {
-                            ForEach(0 ..< tipPercentages.count) {
-                                Text("\(self.tipPercentages[$0])%")
-                            }
-                        }.pickerStyle(SegmentedPickerStyle())
-                    }
-                    
-                    Section{
-                        Text("$\(String(totalPerPerson))")
-                    }
+                }
+                
+                Section ( header: Text("How Much Tip You Would Like To Leave")){
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section{
+                    Text("$\(String(totalPerPerson))")
                 }
             }
             .navigationBarTitle("Split-It")
         }
+      
     }
 }
 
